bf9fb0440ebef813a05bcfbec9b1672d4e564452
João Prado <joaopedro0128@hotmail.com>
2022-12-16 12:28:10 -0300

    feat: for #5
    Signed-off-by: João Prado <joaopedro0128@hotmail.com>

--- Program.cs
+++ Program.cs
@@ -7,21 +7,16 @@ namespace nelio_dotnet
   {
     static void Main(string[] args)
     {
-      Console.Write("Insira a quantidade: ");
+      Console.Write("Insira o valor: ");
       int n = int.Parse(Console.ReadLine());
 
-      for (int i = 0; i < n; i++)
+      int sum = 1;
+      for (int i = n; i > 0; i--)
       {
-        string[] nums = Console.ReadLine().Split(" ");
-        float den = float.Parse(nums[1]);
-        if (den == 0)
-        {
-          Console.WriteLine("divisão impossível");
-          continue;
-        }
-
-        Console.WriteLine(float.Parse(nums[0]) / den);
+        sum *= i;
       }
+
+      Console.WriteLine(sum);
     }
   }
 }
