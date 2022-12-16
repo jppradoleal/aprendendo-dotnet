aef0caacb99cf4ee48fb71f44c958c6de8d4553a
João Prado <joaopedro0128@hotmail.com>
2022-12-16 11:57:40 -0300

    feat: for #1
    Signed-off-by: João Prado <joaopedro0128@hotmail.com>

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace nelio_dotnet
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.Write("Insira o máximo: ");
+      int x = int.Parse(Console.ReadLine());
+
+      for (int i = 1; i <= x; i += 2)
+      {
+        Console.WriteLine(i);
+      }
+    }
+  }
+}
