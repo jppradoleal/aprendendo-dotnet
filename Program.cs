19b8b7be262e749e1dd6fb9cb17b8c94304d4bc1
João Prado <joaopedro0128@hotmail.com>
2022-12-16 12:21:23 -0300

    feat: for #3
    Signed-off-by: João Prado <joaopedro0128@hotmail.com>

--- Program.cs
+++ Program.cs
@@ -10,22 +10,16 @@ namespace nelio_dotnet
       Console.Write("Insira a quantidade: ");
       int n = int.Parse(Console.ReadLine());
 
-      int inside = 0;
-      int outside = 0;
+      List<float> medias = new List<float>();
 
       for (int i = 0; i < n; i++)
       {
-        int m = int.Parse(Console.ReadLine());
-        if (m >= 10 && m <= 20)
-        {
-          inside++;
-          continue;
-        }
-        outside++;
+        string[] notas = Console.ReadLine().Split(" ");
+        float mediaAtual = (float.Parse(notas[0]) * 2 + float.Parse(notas[1]) * 3 + float.Parse(notas[2]) * 5) / 10;
+        medias.Add(mediaAtual);
       }
 
-      Console.WriteLine($"{inside} in");
-      Console.WriteLine($"{outside} out");
+      Console.WriteLine(String.Join(", ", medias));
     }
   }
 }
