08760b888cf1844f1d8c36587d5f053268b8fbd8
João Prado <joaopedro0128@hotmail.com>
2022-12-16 16:50:24 -0300

    feat: class #1
    Signed-off-by: João Prado <joaopedro0128@hotmail.com>

--- Program.cs
+++ Program.cs
@@ -3,17 +3,42 @@ using System.Collections.Generic;
 
 namespace nelio_dotnet
 {
+  class Pessoa
+  {
+    public string nome;
+    public int idade;
+
+    public Pessoa(string nome, int idade)
+    {
+      this.nome = nome;
+      this.idade = idade;
+    }
+
+
+  }
   class Program
   {
     static void Main(string[] args)
     {
-      Console.Write("Insira as linhas: ");
-      int n = int.Parse(Console.ReadLine());
-
-      for (int i = 1; i <= n; i++)
+      Pessoa[] pessoas = new Pessoa[2];
+      for (int i = 0; i < 2; i++)
       {
-        Console.WriteLine($"{i} {Math.Pow(i, 2)} {Math.Pow(i, 3)}");
+        Console.WriteLine($"Dados do {i + 1}º funcionário");
+        Console.Write("Nome: ");
+        string name = Console.ReadLine();
+        Console.Write("Idade: ");
+        int age = int.Parse(Console.ReadLine());
+        pessoas[i] = new Pessoa(name, age);
       }
+
+      Pessoa older = pessoas[0];
+
+      if (pessoas[1].idade > older.idade)
+      {
+        older = pessoas[1];
+      }
+
+      Console.WriteLine($"Pessoa mais velha: {older.nome}");
     }
   }
 }
