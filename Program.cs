67754503b83850a8ff2b16c72522991c7d178305
João Prado <joaopedro0128@hotmail.com>
2022-12-17 03:06:29 -0300

    feat: class #3
    Signed-off-by: João Prado <joaopedro0128@hotmail.com>

--- Program.cs
+++ Program.cs
@@ -1,37 +1,99 @@
 ﻿using System;
+using System.Globalization;
 using System.Collections.Generic;
 
 namespace nelio_dotnet
 {
-  class Employee
+  class BankAccount
   {
-    public string nome;
-    public float wage;
+    private static double TAX = 5.0;
 
-    public Employee(string nome, float wage)
+    public int accountNumber { get; private set; }
+    public string ownerName;
+    private double balance;
+
+    public BankAccount(int accountNumber, string ownerName, double initialBalance)
     {
-      this.nome = nome;
-      this.wage = wage;
+      this.accountNumber = accountNumber;
+      this.ownerName = ownerName;
+      this.balance = 0;
+      this.Deposit(initialBalance);
     }
 
+    public BankAccount(int accountNumber, string ownerName)
+    {
+      this.accountNumber = accountNumber;
+      this.ownerName = ownerName;
+      this.balance = 0;
+    }
 
+    public Double Withdraw(double amount)
+    {
+      if (amount < 0)
+      {
+        throw new InvalidOperationException("Amount must be greater than 0.");
+      }
+
+      this.balance -= amount + BankAccount.TAX;
+      return this.balance;
+    }
+
+    public Double Deposit(double amount)
+    {
+      if (amount < 0)
+      {
+        throw new InvalidOperationException("Amount must be greater than 0.");
+      }
+
+      this.balance += amount;
+
+      return this.balance;
+    }
+
+    public override string ToString()
+    {
+      return $"Conta: {this.accountNumber}, Titular: {this.ownerName}, Saldo: $ {this.balance:F2}";
+    }
   }
+
   class Program
   {
     static void Main(string[] args)
     {
-      Employee[] employees = new Employee[2];
-      for (int i = 0; i < 2; i++)
+      BankAccount account;
+
+      Console.Write("Entre o número da conta: ");
+      int accountNumber = int.Parse(Console.ReadLine());
+
+      Console.Write("Entre o titular da conta: ");
+      string ownerName = Console.ReadLine();
+
+      Console.Write("Haverá depósito inicial (s/n)? ");
+      if (Console.ReadLine().Equals("s"))
       {
-        Console.WriteLine($"Dados do {i + 1}º funcionário");
-        Console.Write("Nome: ");
-        string name = Console.ReadLine();
-        Console.Write("Salário: ");
-        float wage = float.Parse(Console.ReadLine());
-        employees[i] = new Employee(name, wage);
+        Console.Write("Entre o valor de depósito inicial: ");
+        double initialDeposit = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        account = new BankAccount(accountNumber, ownerName, initialDeposit);
+      }
+      else
+      {
+        account = new BankAccount(accountNumber, ownerName);
       }
 
-      Console.WriteLine($"Salário médio: {(employees[0].wage + employees[1].wage) / 2}");
+      Console.WriteLine("\nDados da conta:");
+      Console.WriteLine(account.ToString());
+
+      Console.Write("\nEntre um valor para depósito: ");
+      double amount = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+      account.Deposit(amount);
+      Console.WriteLine("Dados da conta atualizados:");
+      Console.WriteLine(account.ToString());
+
+      Console.Write("\nEntre um valor para saque: ");
+      amount = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+      account.Withdraw(amount);
+      Console.WriteLine("Dados da conta atualizados:");
+      Console.WriteLine(account.ToString());
     }
   }
 }
